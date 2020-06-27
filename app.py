99564a48d289a432b91f958a72175612336dfc3b
TryLettingGo <40615867+TryLettingGo@users.noreply.github.com>
2020-06-27 19:57:13 -0400

    fixing 404

--- app.py
+++ app.py
@@ -3,8 +3,27 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 app.vars = {}
 
-@app.route('/index', methods = ['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        #request was a POST
+        app.vars['ticker'] = request.form['ticker']
+        app.vars['open'] = request.form['open']
+        app.vars['close'] = request.form['close']
+        app.vars['high'] = request.form['high']
+        app.vars['low'] = request.form['low']
+
+        f = open('test.txt','w')
+        f.write('ticker: %s\n'%(app.vars['ticker']))
+        f.write('open: %s\n\n'%(app.vars['open']))
+        f.write('close: %s\n\n'%(app.vars['close']))
+        f.write('high: %s\n\n'%(app.vars['high']))
+        f.write('low: %s\n\n'%(app.vars['low']))
+        f.close()
+
+        return 'request.method was not a GET!'
     return render_template('index.html')
 
 
