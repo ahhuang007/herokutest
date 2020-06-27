885f4f9151e3aa1205987743bfc653fd5ad2af74
TryLettingGo <40615867+TryLettingGo@users.noreply.github.com>
2020-06-27 19:47:42 -0400

    fiddling with html p2

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.vars = {}
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507, debug = True)
