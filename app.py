275d50ea4e4937f75901fc81039669a909dac402
TryLettingGo <40615867+TryLettingGo@users.noreply.github.com>
2020-06-28 00:08:16 -0400

    fixed checkboxes and ticker

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-import pandas as pd
+
 app = Flask(__name__)
 app.vars = {}
 
@@ -11,19 +11,19 @@ def index():
         #request was a POST
         app.vars['ticker'] = request.form['ticker']
         selected = request.form.getlist('op')
-        any_sel = bool(selected)
+        #any_sel = bool(selected)
         #app.vars['open'] = request.form['open']
         #app.vars['close'] = request.form['close']
         #app.vars['high'] = request.form['high']
         #app.vars['low'] = request.form['low']
-        
+        print(selected)
         
         #f.write('open: %s\n\n'%(app.vars['open']))
         #f.write('close: %s\n\n'%(app.vars['close']))
         #f.write('high: %s\n\n'%(app.vars['high']))
         #f.write('low: %s\n\n'%(app.vars['low']))
 
-        return 'any_sel'
+        return 'request.method was not a GET!'
     
 
 
